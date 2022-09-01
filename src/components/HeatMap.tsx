ebca992e68b67ad5cb00725353c2ba4fdfb4b97f
Yli-Hillilä <maanyli@jyu.fi>
2022-09-01 19:53:31 +0300

    added legend bar to solutions; solution swapping function; solution goodness to tooltip

--- src/components/HeatMap.tsx
+++ src/components/HeatMap.tsx
@@ -7,6 +7,7 @@ import { ScenarioBasedSolutionCollection } from "../types/ProblemTypes"
 import { pointer } from "d3-selection";
 import "d3-transition";
 import "./Svg.css";
+import { extent } from "d3-array";
 
 interface HeatMapProps {
     solutions : ScenarioBasedSolutionCollection;
@@ -19,21 +20,35 @@ const HeatMap = ({solutions} : HeatMapProps) => {
     const defaultDimensions = {
         width: 300,
         height: 300,
-        margin: {top: 20, right: 20, bottom: 20, left: 20}
+        margin: {top: 80, right: 20, bottom: 20, left: 20}
     }       
 
+    const switchSolutions = (i: number, j: number) => {
+        [solutions.solutions[i], solutions.solutions[j]] = [solutions.solutions[j], solutions.solutions[i]];
+    }
+
     useEffect(() => {
-        const svgContainer = select(ref.current)
+
+        //switchSolutions(0,1);
+        
             //.classed('component-container', true);
 
         //svgContainer.selectAll('*').remove();
-        
-        const svg = svgContainer
+
+        for (let i = 0; i < solutions.solutions.length; i++) {
+            const svgContainer = select(ref.current)
+            const svg = svgContainer
             .append('svg')
             .classed('svg-content', true)
             .attr('width', defaultDimensions.width + defaultDimensions.margin.left + defaultDimensions.margin.right)
             .attr('height', defaultDimensions.height + defaultDimensions.margin.top + defaultDimensions.margin.bottom)
-            .attr("transform", `translate(${defaultDimensions.margin.left},${defaultDimensions.margin.top})`);
+            .attr(
+                "transform", 
+                `translate(
+                    ${defaultDimensions.margin.left*(i+1)*3+defaultDimensions.width*i},
+                    ${defaultDimensions.margin.top}
+                )`
+            );
 
         const tooltip = svgContainer
             .append('g')
@@ -52,10 +67,16 @@ const HeatMap = ({solutions} : HeatMapProps) => {
                 tooltip.style('visibility', 'visible');
             }
         const tooltipMousemove = 
-            (event, datum) => {
+            (event : any, datum : any) => {
                 const [x,y] = pointer(event);
+                var percentOfIdealString = '';
+                if (solutions.objectiveIdeals?.get(datum.objectiveId) === undefined) return;
+                else if (solutions.objectivesToMaximize.get(datum.objectiveId))
+                    percentOfIdealString = `solution/ideal (maximizing): ${(datum.objectiveValue / solutions.objectiveIdeals?.get(datum.objectiveId)).toFixed(2)}`;
+                else 
+                    percentOfIdealString = `ideal/solution (minimizing): ${(solutions.objectiveIdeals?.get(datum.objectiveId) / datum.objectiveValue).toFixed(2)}`;
                 tooltip
-                    .html(`Value: ${datum.objectiveValue.toString()}`)
+                    .html(`Value: ${datum.objectiveValue.toString()}; ${percentOfIdealString}`)
                     .style('left', `${x+20}px`)
                     .style('top', `${y-10}px`);
                     //.attr('transform', `translate(${x},${y})`);
@@ -80,30 +101,86 @@ const HeatMap = ({solutions} : HeatMapProps) => {
             .padding(0.01);
         const yAxis = axisLeft(yScale);
         svg.append("g")
+            //.attr('transform', `translate(${defaultDimensions.margin.left},0)`)
+            //.attr('x', -60)
+            //.style('fill', 'red')
             .call(yAxis);
 
         if (svg === undefined) console.log('svg undefined');
-        else{
 
-        
+            
         svg.selectAll()
-            .data(solutions.solutions[0], d => d)
+            .data(solutions.solutions[i])
             .enter()
             .append('rect')
             .attr('x', datum => xScale(datum.scenarioId))
             .attr('y', datum => yScale(datum.objectiveId))
             .attr('width', xScale.bandwidth())
             .attr('height', yScale.bandwidth())
-            .style('fill', datum => interpolateBlues(datum.objectiveValue))
+            .style('fill', datum => {
+                if (solutions.objectivesToMaximize.get(datum.objectiveId)) 
+                    // TODO: calculate solutions.objectiveIdeals from the biggest/smallest value if not provided
+                    return interpolateBlues(datum.objectiveValue / solutions.objectiveIdeals?.get(datum.objectiveId));
+                else 
+                    return interpolateBlues(solutions.objectiveIdeals?.get(datum.objectiveId) / datum.objectiveValue);
+            })
             .on('mousemove', tooltipMousemove)
             .on('mouseleave', tooltipMouseleave)
             .on('mouseover', tooltipMouseover)
             /**/
+
+        const legendColors = [interpolateBlues(1), interpolateBlues(0)];
+        // TODO: what does this actually do?
+        //var todoChangeNameOfThis = extent(legendColors, d => d.value);
+        /*
+
+        const legendDimensions = {
+            width: 28,
+            height: 84,
+            margin: {top: 20, right: 20, bottom: 20, left: 20}
+        };
+        
+        const legend = svgContainer
+            .append('defs')
+            .append('linearGradient')
+        */
+
+        const legendGradient = svg.append('defs')
+            .append('linearGradient')
+            .attr('id', 'legendGradient')
+            .attr('x1', '0%')
+            .attr('x2', '0%')
+            .attr('y1', '0%')
+            .attr('y2', '100%');
+
+        legendGradient.append('stop')
+            .attr('class', 'start')
+            .attr('offset', '0%')
+            .attr('stop-color', legendColors[0])
+            .attr('stop-opacity', 1);
+
+        legendGradient.append('stop')
+            .attr('class', 'end')
+            .attr('offset', '100%')
+            .attr('stop-color', 'blue')
+            .attr('stop-opacity', 1);
+
+        svg.append('g')
+            //.selectAll('g')
+            //.data(legendColors)
+            //.enter()
+            .append('rect')
+            .attr('width', 28)
+            .attr('height', 64)
+            .attr('x', defaultDimensions.width + defaultDimensions.margin.right)
+            .attr('y', defaultDimensions.height/2 - 64/2)
+            .style('fill', 'url(#legendGradient)')
         }
+        
+        
     });
 
     return <div ref={ref} id="container" className="component-container"/>
-    //return <svg ref={ref} width={svgWidth} height={svgHeight} />;
 }; 
 
 export default HeatMap;
