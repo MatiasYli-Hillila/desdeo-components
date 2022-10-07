54a722a205b84b9495b987074abf75fd40f04735
Matias Yli-Hillilä <maanyli@jyu.fi>
2022-10-07 13:05:04 +0300

    HeatMap: reindenting, prettied comments

--- src/components/HeatMap.tsx
+++ src/components/HeatMap.tsx
@@ -12,7 +12,7 @@ import { ScenarioBasedObjectiveValue, ScenarioBasedSolution, ScenarioBasedSoluti
 import "./Svg.css";
 
 interface solutionDimensions {
-    width: number, 
+    width: number,
     height: number,
     margin: {
         top: number,
@@ -33,34 +33,34 @@ const HeatMap = ({solutionCollection, solutionDimensions} : HeatMapProps) => {
         width: 300,
         height: 300,
         margin: {
-            top: 80, 
-            right: 20, 
-            bottom: 20, 
+            top: 80,
+            right: 20,
+            bottom: 20,
             left: 80
         }
     };
-    
-    // TODO: this goes over column 120, but reindenting breaks if it's not oneliner
+
+    // TODO: This goes over column 120, but reindenting breaks if it's not oneliner
     const [solutionDimensionsState, setSolutionDimensionsState] = useState(solutionDimensions ? solutionDimensions : solutionDefaultDimensions);
     const [solutionsState, setSolutionsState] = useState(solutionCollection.solutions);
     const [removedSolutionsState, setRemovedSolutionsState] = useState(Array<ScenarioBasedSolution>());
     const [scenarioIdsState, setScenarioIdsState] = useState(solutionCollection.scenarioIds);
     const [objectiveIdsState, setObjectiveIdsState] = useState(solutionCollection.objectiveIds);
-    
-    // TODO: generalize the index swap functions, they all do the same to different arrays
+
+    // TODO: Generalize the index swap functions, they all do the same to different arrays
     //#region index swap functions
-    
-    // TODO: rename swapSolutions to swapSolutionIndices
+
+    // TODO: Rename swapSolutions to swapSolutionIndices
     /**
-    * Swaps indices i, j of solutionsState. 
+    * Swaps indices i, j of solutionsState.
     */
     const swapSolutions = (i: number, j: number) => {
         const solutionsStateCopy = [...solutionsState];
         [solutionsStateCopy[i], solutionsStateCopy[j]] = [solutionsStateCopy[j], solutionsStateCopy[i]];
         setSolutionsState(solutionsStateCopy);
     };
-    
-    // TODO: rename swapScenariosIndices to swapScenarioIndices
+
+    // TODO: Rename swapScenariosIndices to swapScenarioIndices
     /**
     * Swaps indices i, j of scenarioIdsState.
     */
@@ -69,7 +69,7 @@ const HeatMap = ({solutionCollection, solutionDimensions} : HeatMapProps) => {
         [scenarioIdsStateCopy[i], scenarioIdsStateCopy[j]] = [scenarioIdsStateCopy[j], scenarioIdsStateCopy[i]];
         setScenarioIdsState(scenarioIdsStateCopy);
     };
-    
+
     /**
     * Swaps indices i, j of objectiveIdsState.
     */
@@ -78,13 +78,13 @@ const HeatMap = ({solutionCollection, solutionDimensions} : HeatMapProps) => {
         [objectiveIdsStateCopy[i], objectiveIdsStateCopy[j]] = [objectiveIdsStateCopy[j], objectiveIdsStateCopy[i]];
         setObjectiveIdsState(objectiveIdsStateCopy);
     };
-    
+
     //#endregion
-    
+
     useEffect(() => setSolutionsState(solutionCollection.solutions), [solutionCollection.solutions]);
     useEffect(() => setScenarioIdsState(solutionCollection.scenarioIds), [solutionCollection.scenarioIds]);
     useEffect(() => setObjectiveIdsState(solutionCollection.objectiveIds), [solutionCollection.objectiveIds]);
-    
+
     // TODO: Make these into their own object, move to another file, something else? Remove useRef? Generalize?
     var mouseoveredSolutionIndex = useRef<number>(null!);
     var currentDraggedSolutionIndex = useRef<number>(null!);
@@ -92,18 +92,18 @@ const HeatMap = ({solutionCollection, solutionDimensions} : HeatMapProps) => {
     var currentDraggedScenarioIndex = useRef<number>(null!);
     var mouseoveredObjectiveIndex = useRef<number>(null!);
     var currentDraggedObjectiveIndex = useRef<number>(null!);
-    
+
     /* Render the component */
     useEffect(() => {
-        
+
         const renderW = solutionDimensionsState.width + solutionDimensionsState.margin.left + solutionDimensionsState.margin.right;
         const renderH = solutionDimensionsState.height + solutionDimensionsState.margin.bottom + solutionDimensionsState.margin.top;
-        
+
         const svgContainer = select(ref.current);
         svgContainer.selectAll('*').remove();
-        
+
         //#region tooltip
-        
+
         const tooltip = svgContainer
         .append('g')
         .classed('tooltip', true)
@@ -116,10 +116,10 @@ const HeatMap = ({solutionCollection, solutionDimensions} : HeatMapProps) => {
         .style('padding', '5px')
         .style('position', 'absolute')
         .style('z-index', 1000);
-        
+
         const tooltipMouseover = () => tooltip.style('visibility', 'visible');
         const tooltipMouseleave = () => tooltip.style('visibility', 'hidden');
-        
+
         const tooltipMousemove = (event : MouseEvent, datum : ScenarioBasedObjectiveValue) => {
             const [x,y] = pointer(event);
             var percentOfIdealString = 'goodness% ';
@@ -130,8 +130,8 @@ const HeatMap = ({solutionCollection, solutionDimensions} : HeatMapProps) => {
                     (datum.objectiveValue / solutionCollection.objectiveIdeals.get(datum.objectiveId)!).toFixed(2)
                 }`;
             }
-            
-            else 
+
+            else
             {
                 percentOfIdealString += `(minimizing): ${(
                     solutionCollection.objectiveIdeals.get(datum.objectiveId)! / datum.objectiveValue).toFixed(2)
@@ -142,19 +142,19 @@ const HeatMap = ({solutionCollection, solutionDimensions} : HeatMapProps) => {
             .style('left', `${x+20}px`)
             .style('top', `${y-10}px`);
         };
-        
+
         //#endregion
-        
+
         //#region legend
-        
+
         const legendColors = [interpolateBlues(0), interpolateBlues(1)];
-        
+
         const legendSVG = svgContainer.append('svg').classed('svg-content', true)
         .attr('width', 100 + 20)
         .attr('height', 400);
         //.style('outline', 'thick solid #FFFFFF');
-        
-        
+
+
         const legendGradient = legendSVG.append('defs')
         .append('linearGradient')
         .attr('id', 'legendGradient')
@@ -162,19 +162,19 @@ const HeatMap = ({solutionCollection, solutionDimensions} : HeatMapProps) => {
         .attr('x2', '0%')
         .attr('y1', '0%')
         .attr('y2', '100%');
-        
+
         legendGradient.append('stop')
         .attr('class', 'start')
         .attr('offset', '0%')
         .attr('stop-color', legendColors[1])
         .attr('stop-opacity', 1);
-        
+
         legendGradient.append('stop')
         .attr('class', 'end')
         .attr('offset', '100%')
         .attr('stop-color', legendColors[0])
         .attr('stop-opacity', 1);
-        
+
         legendSVG//.append('g')
         //.classed('svg-content', true)
         //.attr('width', renderW)
@@ -188,26 +188,26 @@ const HeatMap = ({solutionCollection, solutionDimensions} : HeatMapProps) => {
         //.attr('y', solutionDimensionsState.height/2 - 64/2)
         .style('fill', 'url(#legendGradient)')
         .style('outline', 'thin solid #000000');
-        
-        
+
+
         legendSVG.append('text')
-        .attr("x", 50)             
+        .attr("x", 50)
         .attr("y", 20)
-        .style("text-anchor", "middle") 
-        .style("font-size", "16px") 
+        .style("text-anchor", "middle")
+        .style("font-size", "16px")
         .text(() => 'ideal')
         .style('fill', legendColors[0])
-        
+
         legendSVG.append('text')
-        .attr("x", 50)             
+        .attr("x", 50)
         .attr("y", 390)
-        .style("text-anchor", "middle") 
-        .style("font-size", "16px") 
+        .style("text-anchor", "middle")
+        .style("font-size", "16px")
         .text(() => 'nadir')
         .style('fill', legendColors[1])
-        
+
         //#endregion
-        
+
         //#region removed solutions list
 
         const addSolutionBack = (event: MouseEvent) => {
@@ -224,7 +224,7 @@ const HeatMap = ({solutionCollection, solutionDimensions} : HeatMapProps) => {
                 ]);
             }
         };
-        
+
         // TODO: Use different class? Make this work better (i.e. margins); ADD OUTLINE.
         const removedSolutionsList = svgContainer.append('g').classed('svg-content', true)
         //.attr('transform', `translate(${solutionDimensionsState.margin.left}, 0`)
@@ -245,16 +245,16 @@ const HeatMap = ({solutionCollection, solutionDimensions} : HeatMapProps) => {
         //.attr('height', 400)
         //.attr('x', 0)
         .attr('y', 50);
-        
+
         removedSolutionsList.selectAll('li')
         .data(removedSolutionsState)
         .enter()
         .append('li')
         .on('click', mouseEvent => addSolutionBack(mouseEvent))
         .text(d => d.solutionId);
-        
+
         //#endregion
-        
+
         for (let i = 0; i < solutionsState.length; i++) {
             const svg = svgContainer
             .append('svg')
@@ -262,76 +262,76 @@ const HeatMap = ({solutionCollection, solutionDimensions} : HeatMapProps) => {
             .attr('id', solutionsState[i].solutionId)
             .attr('width', renderW)
             .attr('height', renderH)
-            
+
             if (svg === undefined) console.log('svg undefined');
-            
-            // TODO: refactor mouseover functions to be more general, they have copypasted code
+
+            // TODO: Refactor mouseover functions to be more general, they have copypasted code
             const solutionMouseover = (event: MouseEvent) => {
                 const target = event.target as HTMLElement;
                 const solutionId = target.classList.value;
                 const solutionIndex = solutionsState.findIndex(i => i.solutionId === solutionId);
                 mouseoveredSolutionIndex.current = solutionIndex;
             };
-            
+
             const solutionDragStart = (dragStartobject: any) => {
                 const solutionId = dragStartobject.sourceEvent.target.classList.value;
                 const solutionIndex = solutionsState.findIndex(i => i.solutionId === solutionId);
                 currentDraggedSolutionIndex.current = solutionIndex;
             };
-            
+
             const solutionDragEnd = () => {
                 if (mouseoveredSolutionIndex.current === null || currentDraggedSolutionIndex.current === null) return;
                 else swapSolutions(currentDraggedSolutionIndex.current, mouseoveredSolutionIndex.current);
             };
-            
+
             const solutionDrag = drag()
             .on('start', solutionDragStart)
             .on('end', solutionDragEnd);
-            
+
             const scenarioMouseover = (event: MouseEvent) => {
                 var target = event.target as HTMLElement;
                 const scenarioId = target.classList.value;
                 const scenarioIndex = scenarioIdsState.findIndex(i => i === scenarioId);
                 mouseoveredScenarioIndex.current = scenarioIndex;
             };
-            
+
             const scenarioDragStart = (dragStartObject: any) => {
                 const scenarioId = dragStartObject.sourceEvent.target.classList.value;
                 const scenarioIndex = scenarioIdsState.findIndex(i => i === scenarioId);
                 currentDraggedScenarioIndex.current = scenarioIndex;
             };
-            
+
             const scenarioDragEnd = () => {
                 if (mouseoveredScenarioIndex.current === null || currentDraggedScenarioIndex.current === null) return;
                 else swapScenariosIndices(mouseoveredScenarioIndex.current, currentDraggedScenarioIndex.current);
             };
-            
+
             const scenarioDrag = drag()
             .on('start', scenarioDragStart)
             .on('end', scenarioDragEnd);
-            
+
             const objectiveMouseover = (event: MouseEvent) => {
                 var target = event.target as HTMLElement;
                 const objectiveId = target.classList.value;
                 const objectiveIndex = objectiveIdsState.findIndex(i => i === objectiveId);
                 mouseoveredObjectiveIndex.current = objectiveIndex;
             };
-            
+
             const objectiveDragStart = (dragStartObject: any) => {
                 const objectiveId = dragStartObject.sourceEvent.target.classList.value;
                 const objectiveIndex = objectiveIdsState.findIndex(i => i === objectiveId);
                 currentDraggedObjectiveIndex.current = objectiveIndex;
             };
-            
+
             const objectiveDragEnd = () => {
                 if (mouseoveredObjectiveIndex.current === null || currentDraggedObjectiveIndex.current === null) return;
                 else swapObjectiveIndices(mouseoveredObjectiveIndex.current, currentDraggedObjectiveIndex.current);
             };
-            
+
             const objectiveDrag = drag()
             .on('start', objectiveDragStart)
             .on('end', objectiveDragEnd);
-            
+
             const removeSolution = (event: MouseEvent) => {
                 const eventTarget = event.target as HTMLElement;
                 const solutionId = eventTarget.parentElement?.id;
@@ -346,16 +346,16 @@ const HeatMap = ({solutionCollection, solutionDimensions} : HeatMapProps) => {
                     setRemovedSolutionsState(state => [...state, solutionToRemove]);
                 }
             }
-            
-            
-            
+
+
+
             const xScale = scaleBand()
             .range([0, solutionDimensionsState.width])
             .domain(scenarioIdsState)
             .padding(0.01);
             const xAxis = axisBottom(xScale);
-            
-            // TODO: refactor ts-ignore away
+
+            // TODO: Refactor ts-ignore away
             // @ts-ignore
             svg.append("g")
             .attr("transform", `translate(${solutionDimensionsState.margin.left},${solutionDimensionsState.height + solutionDimensionsState.margin.top})`)
@@ -363,17 +363,17 @@ const HeatMap = ({solutionCollection, solutionDimensions} : HeatMapProps) => {
             .selectAll('text')
             .attr('class', d => d)
             .on('mouseover', scenarioMouseover)
-            // TODO: refactor ts-ignore away
+            // TODO: Refactor ts-ignore away
             // @ts-ignore
             .call(scenarioDrag);
-            
+
             const yScale = scaleBand()
             .range([solutionDimensionsState.height, 0])
             .domain(objectiveIdsState)
             .padding(0.01);
             const yAxis = axisLeft(yScale);
-            
-            // TODO: refactor ts-ignore away
+
+            // TODO: Refactor ts-ignore away
             // @ts-ignore
             svg.append("g")
             .attr('transform', `translate(${solutionDimensionsState.margin.left},${solutionDimensionsState.margin.top})`)
@@ -381,20 +381,20 @@ const HeatMap = ({solutionCollection, solutionDimensions} : HeatMapProps) => {
             .selectAll('text')
             .attr('class', d => d)
             .on('mouseover', objectiveMouseover)
-            // TODO: refactor ts-ignore away
+            // TODO: Refactor ts-ignore away
             // @ts-ignore
             .call(objectiveDrag);
-            
+
             svg.append('text')
-            .attr("x", (solutionDimensionsState.width / 2))             
+            .attr("x", (solutionDimensionsState.width / 2))
             .attr("y", (solutionDimensionsState.margin.top / 2))
-            .style("text-anchor", "middle") 
-            .style("font-size", "16px") 
+            .style("text-anchor", "middle")
+            .style("font-size", "16px")
             .text(() => solutionsState[i].solutionId.toString())
             .style('fill', 'black')
             .on('click', removeSolutionEvent => removeSolution(removeSolutionEvent))
-            
-            // TODO: refactor ts-ignore away
+
+            // TODO: Refactor ts-ignore away
             // @ts-ignore
             svg.selectAll()
             .append('g')
@@ -402,47 +402,44 @@ const HeatMap = ({solutionCollection, solutionDimensions} : HeatMapProps) => {
             .enter()
             .append('rect')
             .attr('class', solutionsState[i].solutionId)
-            // TODO: refactor ts-ignore away
+            // TODO: Refactor ts-ignore away
             // @ts-ignore
             .attr('x', datum => xScale(datum.scenarioId) + solutionDimensionsState.margin.left)
-            // TODO: refactor ts-ignore away
+            // TODO: Refactor ts-ignore away
             // @ts-ignore
             .attr('y', datum => yScale(datum.objectiveId) + solutionDimensionsState.margin.top)
             .attr('width', xScale.bandwidth())
             .attr('height', yScale.bandwidth())
             .style('fill', datum => {
-                if (solutionCollection.objectivesToMaximize.get(datum.objectiveId)) 
-                // TODO: calculate solutions.objectiveIdeals from the biggest/smallest value if not provided
-                // TODO: use Math.abs instead
-                return interpolateBlues(
-                    // TODO: refactor ts-ignore away.
+                if (solutionCollection.objectivesToMaximize.get(datum.objectiveId)) return interpolateBlues(
+                    // TODO: Calculate solutions.objectiveIdeals from the biggest/smallest value if not provided
+                    // TODO: Use Math.abs instead?
+                    // TODO: Refactor ts-ignore away.
                     // @ts-ignore
-                    (datum.objectiveValue - solutionCollection.objectiveNadirs.get(datum.objectiveId)) / 
+                    (datum.objectiveValue - solutionCollection.objectiveNadirs.get(datum.objectiveId)) /
                     // @ts-ignore
                     (solutionCollection.objectiveIdeals.get(datum.objectiveId) - solutionCollection.objectiveNadirs.get(datum.objectiveId))
-                    );
-                    else 
-                    return interpolateBlues(
+                ); else return interpolateBlues(
                         // @ts-ignore
-                        (datum.objectiveValue - solutionCollection.objectiveNadirs.get(datum.objectiveId)) / 
+                        (datum.objectiveValue - solutionCollection.objectiveNadirs.get(datum.objectiveId)) /
                         // @ts-ignore
                         (solutionCollection.objectiveIdeals.get(datum.objectiveId) - solutionCollection.objectiveNadirs.get(datum.objectiveId))
-                        );
-                    })
-                    .on('mousemove', tooltipMousemove)
-                    .on('mouseleave', tooltipMouseleave)
-                    .on('mouseover', (d) => {
-                        tooltipMouseover(); 
-                        solutionMouseover(d);
-                    })
-                    // TODO: refactor ts-ignore away
-                    // @ts-ignore
-                    .call(solutionDrag);
+                );
+            })
+            .on('mousemove', tooltipMousemove)
+            .on('mouseleave', tooltipMouseleave)
+            .on('mouseover', (d) => {
+                tooltipMouseover();
+                solutionMouseover(d);
+            })
+            // TODO: Refactor ts-ignore away
+            // @ts-ignore
+            .call(solutionDrag);
         };
-                
+
     });
-            
+
     return <div ref={ref} id="container" className="component-container"/>
-}; 
-        
+};
+
 export default HeatMap;
